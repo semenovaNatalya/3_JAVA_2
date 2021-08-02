58cf467a08e67a4e5a89c97c01782244ce7c23f3
Semenova Natalya <semnazl@gmail.com>
2021-08-03 00:22:29 +0300

    error correction

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int initial_amount;   //начальная сумма на счете клиента
+        int initialAmount;   //начальная сумма на счете клиента
         int refill;           //сумма пополнения счета
         int bonus;            //количество начисляемых бонусов
-        int current_balance;  //итоговый баланс клиента
-        initial_amount = 100;  //входные данные_1
+        int currentBalance;  //итоговый баланс клиента
+        int limitBonus = 1000;
+        initialAmount = 100;  //входные данные_1
         refill = 1100;          //входные данные_2
-        if (refill >= 1000) {
-            bonus = 1 * refill / 100;
-            current_balance = initial_amount + refill + bonus;
-            System.out.println("Ваши бонусы");
-            System.out.println(bonus);
-            System.out.println("Ваш текущий баланс");
-            System.out.println(current_balance);
+        if (refill > limitBonus) {
+            bonus = refill / 100;
         } else {
-            current_balance = initial_amount + refill;
-            System.out.println("Бонусы Вам не начислены");
-            System.out.println("Ваш текущий баланс");
-            System.out.println(current_balance);
+            bonus = 0;
         }
+        currentBalance = initialAmount + refill + bonus;
+        System.out.println(bonus);
+        System.out.println(currentBalance);
     }
 
 }
